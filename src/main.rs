cb65aa3cc8829886f4815a200f6e94d2e9ffd645
Jiho Lee <optional.int@kakao.com>
2024-07-30 15:32:13 +0900

    chore: Improve comment

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,10 @@ fn main() {
     let mut svc = http_proxy_service(&server.configuration, DynamicGateway {});
     // proxy port: 8080
     // mgmt port: 8081
-    //   -> admin api (prefix, service redis hset)
+    //   -> admin api (redis hset by prefix & k8s service name)
     //   -> pingora: forward 0.0.0.0:8081 -> 127.0.0.1:8091
-    //   -> axum: implement api & 127.0.0.0:8091 listen
-    //   * run pingora & axum in same function: main()
+    //   -> axum: implement api & listen on 127.0.0.0:8091
+    //   * Run pingora & axum in same function: main()
     svc.add_tcp("0.0.0.0:8080");
 
     server.add_service(svc);
